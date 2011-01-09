3ab93213b206b445f45ff73a52c7adc5131801c2
F1ash <kaperang07@gmail.com>
2011-01-10 01:29:40 +0300

    new variation
    Signed-off-by: F1ash <kaperang07@gmail.com>

--- contents/code/clnt.py
+++ contents/code/clnt.py
@@ -2,6 +2,10 @@ from xmlrpclib import ServerProxy
 from threading import Thread
 import os, os.path, tarfile
 
+class dataRendering:
+	def __init__(self, destinationFile = None, destinationList = None, turn = True):
+		pass
+
 class xr_client:
 	def __init__(self):
 		pass
@@ -18,7 +22,7 @@ class xr_client:
 		for name in fileList :
 			with open(str(name + '.my.tar'), "wb") as handle:
 				handle.write(s.python_logo(name).data)
-			s.python_clean(name)
+			s.python_clean(str(name + '.tar'))
 
 		for name in fileList :
 			tar = tarfile.open(str(name + '.my.tar'), 'r')
@@ -26,6 +30,35 @@ class xr_client:
 			tar.close()
 			os.remove(str(name + '.my.tar'))
 
+	def run_(self):
+		servaddr = 'http://localhost:35113'
+		s = ServerProxy(servaddr)
+
+		methods = s.system.listMethods()
+		os.chdir('/tmp')
+		fileList = ["python_logo.jpg", 'clnt.py', 'iy']
+		for name in fileList :
+			if s.typePath(name) :
+				print True
+				with open('/dev/shm/struct_', "wb") as handle:
+					handle.write(s.requistCatalogStruct(name).data)
+				tar = tarfile.open('/dev/shm/_struct', 'r')
+				tar.extractall()
+				tar.close()
+				s.python_clean('/dev/shm/_struct')
+				f = open(str(os.getcwd() + '/dev/shm/_listFiles'), 'rb')
+				catalogFileList = f.readlines()
+				f.close()
+				os.remove(str(os.getcwd() + '/dev/shm/_listFiles'))
+				os.remove(str('/dev/shm/struct_'))
+				for name in catalogFileList :
+					with open(name, "wb") as handle:
+						handle.write(s.python_file(name).data)
+			else :
+				print False
+				with open(name, "wb") as handle:
+					handle.write(s.python_file(name).data)
+
 if __name__ == '__main__':
 	t = xr_client()
-	t._run()
+	t.run_()
--- contents/code/serv.py
+++ contents/code/serv.py
@@ -5,6 +5,23 @@ from SocketServer import ThreadingMixIn, ForkingMixIn
 
 class ThreadServer(ThreadingMixIn, DocXMLRPCServer):pass
 
+def typePath(name):
+	if os.path.isdir(name) :
+		return True
+	else :
+		return False
+
+def readCatalogStruct(name, tarObj, listObj):
+	tar = tarObj
+	listFile = listObj
+	print name
+	tar.add(name, None, False)
+	for name_ in os.listdir(name):
+		if os.path.isdir(name_) :
+			readCatalogStruct(name_, tar, listFile)
+		else :
+			listFile += [name_]
+
 def parseFileList(name, tarObj) :
 	tar = tarObj
 	if os.path.isdir(name) :
@@ -14,7 +31,7 @@ def parseFileList(name, tarObj) :
 		tar.add(name)
 
 def python_clean(name):
-	os.remove(str(name + '.tar'))
+	os.remove(name)
 
 def python_logo(name):
 	#os.chdir('/tmp')
@@ -23,6 +40,24 @@ def python_logo(name):
 	tar.add(name)
 	tar.close()
 	with open(str(name + '.tar'), "rb") as handle:
+		return xmlrpclib.Binary(handle.read())
+
+def python_file(name):
+	#os.chdir('/tmp')
+	with open(name, "rb") as handle:
+		return xmlrpclib.Binary(handle.read())
+
+def requistCatalogStruct(name):
+	listCatalogFiles = []
+	f1 = tarfile.open('/dev/shm/_struct', 'w|bz2')
+	readCatalogStruct(name, f1, listCatalogFiles)
+	f2 = open('/dev/shm/_listFiles', 'wb')
+	f2.writelines(listCatalogFiles)
+	f2.close()
+	f1.add('/dev/shm/_listFiles')
+	f1.close()
+	os.remove('/dev/shm/_listFiles')
+	with open('/dev/shm/_struct', "rb") as handle:
 		return xmlrpclib.Binary(handle.read())
 
 class Daemon:
@@ -36,6 +71,9 @@ class Daemon:
 		self._srv.register_introspection_functions()
 		self._srv.register_function(python_logo, 'python_logo')
 		self._srv.register_function(python_clean, 'python_clean')
+		self._srv.register_function(typePath, 'typePath')
+		self._srv.register_function(python_file, 'python_file')
+		self._srv.register_function(requistCatalogStruct, 'requistCatalogStruct')
 
 	def run(self):
 		self._srv.serve_forever()
