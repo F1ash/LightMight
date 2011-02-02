b5d9807b059fd6e1799da0b3cbdc666438892370
F1ash <kaperang07@gmail.com>
2011-02-02 22:11:57 +0300

    backup current state
    Signed-off-by: F1ash <kaperang07@gmail.com>

--- contents/code/LightMight.py
+++ contents/code/LightMight.py
@@ -33,13 +33,13 @@ class MainWindow(QtGui.QMainWindow):
 		listHelp.setStatusTip('Read help')
 		self.connect(listHelp,QtCore.SIGNAL('triggered()'), self.showHelp_)
 
-		setPath = QtGui.QAction(QtGui.QIcon('../icons/help.png'),'&set Path', self)
-		setPath.setStatusTip('Read help')
-		self.connect(setPath, QtCore.SIGNAL('triggered()'), self.showHelp_)
+		serverSettings = QtGui.QAction(QtGui.QIcon('../icons/help.png'),'&Client Settings', self)
+		serverSettings.setStatusTip('Read help')
+		self.connect(serverSettings, QtCore.SIGNAL('triggered()'), self.showServerSettingsShild)
 
-		setAddr = QtGui.QAction(QtGui.QIcon('../icons/help.png'),'&set Address', self)
-		setAddr.setStatusTip('Read help')
-		self.connect(setAddr, QtCore.SIGNAL('triggered()'), self.showHelp_)
+		clientSettings = QtGui.QAction(QtGui.QIcon('../icons/help.png'),'&Server Settings', self)
+		clientSettings.setStatusTip('Read help')
+		self.connect(clientSettings, QtCore.SIGNAL('triggered()'), self.showClientSettingsShild)
 
 		self.statusBar()
 
@@ -51,15 +51,15 @@ class MainWindow(QtGui.QMainWindow):
 		file_.addAction(self.exit_)
 
 		set_ = menubar.addMenu('&Settings')
-		set_.addAction(setPath)
-		set_.addAction(setAddr)
+		set_.addAction(serverSettings)
+		set_.addAction(clientSettings)
 
 		help_ = menubar.addMenu('&Help')
 		help_.addAction(listHelp)
 
-		self.menuTab = BoxLayout(Box)
+		self.menuTab = Box()
 		#self.menuTab = BoxLayout(Box)
-		self.menuTab.setMaximumSize(800, 650)
+		#self.menuTab.setMaximumSize(800, 650)
 		#self.menuTab.clear()
 		#self.menuTab.setUsesScrollButtons(True)
 		#self.create_menuTab(0)
@@ -77,7 +77,8 @@ class MainWindow(QtGui.QMainWindow):
 		self.trayIcon.setToolTip('LightMight')
 		self.trayIcon.setContextMenu(self.trayIconMenu)
 		self.trayIcon.setIcon(QtGui.QIcon(self.trayIconPixmap))
-		self.connect(self.trayIcon, QtCore.SIGNAL("activated(QSystemTrayIcon::ActivationReason)"), self.trayIconClicked)
+		self.connect(self.trayIcon, QtCore.SIGNAL("activated(QSystemTrayIcon::ActivationReason)"), \
+																			self.trayIconClicked)
 		self.trayIcon.show()
 
 		# toolbar = self.addToolBar('Exit')
@@ -108,8 +109,71 @@ class MainWindow(QtGui.QMainWindow):
 		showHelp = ListingText("MSG: LightMight.help", self)
 		showHelp.exec_()
 
-class BoxLayout(QtGui.QWidget):
+	def showClientSettingsShild(self):
+		_ClientSettingsShild = ClientSettingsShild()
+		_ClientSettingsShild.exec_()
+		pass
+
+	def showServerSettingsShild(self):
+		_ServerSettingsShild = ServerSettingsShild()
+		_ServerSettingsShild.exec_()
+		pass
+
+class ClientSettingsShild(QtGui.QDialog):
+	def __init__(self, parent = None):
+		QtGui.QWidget.__init__(self, parent)
+
+		self.setWindowTitle('LightMight Server Settings')
+		self.setWindowIcon(QtGui.QIcon('../icons/tux_partizan.png'))
+
+	def closeEvent(self, event):
+		event.ignore()
+		self.done(0)
+
+
+class ServerSettingsShild(QtGui.QDialog):
+	def __init__(self, parent = None):
+		QtGui.QWidget.__init__(self, parent)
+
+		self.setWindowTitle('LightMight Client Settings')
+		self.setWindowIcon(QtGui.QIcon('../icons/tux_partizan.png'))
+
+	def closeEvent(self, event):
+		event.ignore()
+		self.done(0)
+
+
+class ButtonPanel(QtGui.QWidget):
 	def __init__(self, Obj_, job_key = None, parent = None):
+		QtGui.QWidget.__init__(self, parent)
+
+		self.layout = QtGui.QVBoxLayout()
+		self.layout.addStretch(1)
+
+		self.addButton = QtGui.QPushButton(QtCore.QString('Add'))
+		self.addButton.setToolTip('Add Item to ItemList\nof Shared Source')
+		self.connect(self.addButton, QtCore.SIGNAL('clicked()'), self.addItem)
+		self.layout.addWidget(self.addButton)
+
+		self.delButton = QtGui.QPushButton(QtCore.QString('Del'))
+		self.delButton.setToolTip('Delete Item from ItemList\nof Shared Source')
+		self.connect(self.delButton, QtCore.SIGNAL('clicked()'), self.addItem)
+		self.layout.addWidget(self.delButton)
+
+		self.upLoadButton = QtGui.QPushButton(QtCore.QString('Up'))
+		self.upLoadButton.setToolTip('UpLoad ItemList\nof Shared Source')
+		self.connect(self.upLoadButton, QtCore.SIGNAL('clicked()'), self.addItem)
+		self.layout.addWidget(self.upLoadButton)
+
+		self.setLayout(self.layout)
+
+	def addItem(self):
+		pass
+
+	pass
+
+class BoxLayout(QtGui.QWidget):
+	def __init__(self, Obj_ = None, job_key = None, parent = None):
 		QtGui.QWidget.__init__(self, parent)
 
 		self.setWindowTitle('box layout')
@@ -126,7 +190,6 @@ class BoxLayout(QtGui.QWidget):
 		hbox.addWidget(self.boxTable)
 
 		self.setLayout(vbox)
-		self.setMinimumSize(250, 175)
 
 class ListingText(QtGui.QDialog):
 	def __init__(self, path_, parent = None):
@@ -166,7 +229,7 @@ class TreeItem(QtCore.QObject):
 		self.parentItem = parentItem
 		self.itemData = data
 		self.childItems = []
-		#self.parentItem.children()
+		self.checkState = QtCore.Qt.Unchecked
 
 	def child(self, row):
 		if row <= len(self.childItems):
@@ -179,8 +242,11 @@ class TreeItem(QtCore.QObject):
 	def columnCount(self):
 		return len(self.itemData)
 
-	def data(self, column):
-		return self.itemData[column]
+	def data(self, column, role = QtCore.Qt.DisplayRole):
+		if role == QtCore.Qt.CheckStateRole and column == 0 :
+			return self.checkState
+		elif role != QtCore.Qt.CheckStateRole and column >= 0 :
+			return self.itemData[column]
 
 	def childIndex(self, child_link = None):
 		if child_link in self.childItems and child_link is not None:
@@ -189,7 +255,6 @@ class TreeItem(QtCore.QObject):
 	def row(self):
 		if (self.parentItem is not None):
 			return self.parentItem.childIndex(self)
-			#self.parentItem.childItems.index(self)
 		return None
 
 	def getParentItem(self):
@@ -200,10 +265,12 @@ class TreeItem(QtCore.QObject):
 			self.childItems.append(item)
 
 class TreeModel(QtCore.QAbstractItemModel):
+	checkStateChanged = QtCore.pyqtSignal()
 	def __init__(self, data = [], parent = None):
 		QtCore.QAbstractItemModel.__init__(self, parent)
 
 		self.rootItem = TreeItem(data, None, self)
+		self.changeMe = True
 
 	def index(self, row, column, prnt = QtCore.QModelIndex()):
 		if (not self.hasIndex(row, column, prnt)) :
@@ -257,11 +324,12 @@ class TreeModel(QtCore.QAbstractItemModel):
 		if (not index_.isValid()) :
 			return QtCore.QVariant()
 
-		if (role != QtCore.Qt.DisplayRole) :
-			return QtCore.QVariant()
-
 		item = index_.internalPointer()
-		return item.data(index_.column())
+		if role == QtCore.Qt.CheckStateRole :
+				return item.data(index_.column(), role)
+		elif (role != QtCore.Qt.DisplayRole) :
+			return QtCore.QVariant()
+		return item.data(index_.column(), role)
 
 	def headerData(self, section, orientation = QtCore.Qt.Horizontal, role = QtCore.Qt.DisplayRole):
 		if (orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole) :
@@ -296,6 +364,71 @@ class TreeModel(QtCore.QAbstractItemModel):
 		for child in some_obj.childItems:
 			self.debugPrintObj(child, tab + '	')
 
+	def processCheckStateOfChildren(self, b):
+		qua = b.childCount()
+		checkState = b.checkState
+		for i in range(0, qua):
+			childItem = b.child(i)
+			childItem.checkState = checkState
+			self.processCheckStateOfChildren(childItem)
+
+	def processCheckStateOfParents(self, b):
+		parentItem = b.getParentItem()
+		if parentItem is not None :
+			qua = parentItem.childCount()
+		else :
+			return
+
+		quaOfChecked = 0
+		quaOfPartChecked = 0
+		for i in range(0, qua) :
+			if parentItem.child(i).checkState == QtCore.Qt.Checked :
+				quaOfChecked += 1
+			elif parentItem.child(i).checkState == QtCore.Qt.PartiallyChecked :
+				quaOfPartChecked += 1
+
+			if quaOfChecked == qua :
+				checkState = QtCore.Qt.Checked
+			elif quaOfChecked == 0 and quaOfPartChecked == 0 :
+				checkState = QtCore.Qt.Unchecked
+			else :
+				checkState = QtCore.Qt.PartiallyChecked
+
+			parentItem.checkState = checkState
+			self.processCheckStateOfParents(parentItem)
+
+	def setData(self, index_, value, role = QtCore.Qt.DisplayRole):
+		if not index_.isValid() :
+			return False
+
+		if role == QtCore.Qt.CheckStateRole :
+			item = index_.internalPointer()
+
+			if item.checkState == QtCore.Qt.Unchecked :
+				item.checkState = QtCore.Qt.Checked
+			else :
+				item.checkState = QtCore.Qt.Unchecked
+
+			if self.changeMe and item is not None:
+				self.changeMe = False
+				self.processCheckStateOfChildren(item)
+				self.processCheckStateOfParents(item)
+				self.changeMe = True
+
+			self.emit(QtCore.SIGNAL("checkStateChanged()"))
+			self.emit(QtCore.SIGNAL("dataChanged(const QModelIndex&, const QModelIndex&)"), \
+																	index_.parent(), index_)
+			return True
+
+		return False
+
+	def flags(self, index_):
+		if not index_.isValid() :
+			return QtCore.Qt.ItemIsEnabled
+
+		return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | \
+										QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEditable
+
 class Box(QtGui.QWidget):
 	def __init__(self, job_key = None, parent = None):
 		QtGui.QWidget.__init__(self, parent)
@@ -303,7 +436,8 @@ class Box(QtGui.QWidget):
 		self.layout = QtGui.QGridLayout()
 
 		self.userList = QtGui.QListWidget()
-		self.userList.setMinimumSize(100, 75)
+		self.userList.setMaximumWidth(250)
+		#self.userList.setMinimumSize(100, 75)
 		self.userList.setToolTip('Users in Web')
 		self.layout.addWidget(self.userList, 0, 0)
 
@@ -318,11 +452,15 @@ class Box(QtGui.QWidget):
 		self.sharedTree.setModel(treeModel)
 		self.layout.addWidget(self.sharedTree, 0, 1)
 
+		self.buttunPanel = BoxLayout(ButtonPanel)
+		self.buttunPanel.setMaximumWidth(100)
+		self.layout.addWidget(self.buttunPanel, 0, 2)
+
 		self.setLayout(self.layout)
 
 #os.system('cd $HOME && xauth merge /dev/shm/dsa && rm /dev/shm/dsa')
 name_ = os.path.basename(sys.argv[0])
-print sys.argv[0][:-len(name_)]
+#print sys.argv[0][:-len(name_)]
 os.chdir(sys.argv[0][:-len(name_)])
 app = QtGui.QApplication(sys.argv)
 main = MainWindow()
